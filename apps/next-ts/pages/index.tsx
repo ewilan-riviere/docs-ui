38ec4243e210280b172ef7f97479a2ce07235ec0
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-08-09 14:19:29 +0200

    add app

--- apps/next-ts/pages/index.tsx
+++ apps/next-ts/pages/index.tsx
@@ -1,9 +1,10 @@
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Button } from 'docs-ui-react'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,7 +20,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <Button label="Hey"></Button>
@@ -70,3 +71,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
--- libs/ui/src/components/Button/Button.tsx
+++ libs/ui/src/components/Button/Button.tsx
@@ -1,46 +1,46 @@
 import React from 'react'
 
-// export interface ButtonProps {
-//   label: string
-// }
-
-// const Button = (props: ButtonProps) => {
-//   return <button>{props.label}</button>
-// }
-
-interface ButtonProps {
-  border: string
-  color: string
-  children?: React.ReactNode
-  height: string
-  onClick: () => void
-  radius: string
-  width: string
+export interface ButtonProps {
+  label: string
 }
 
-const Button: React.FC<ButtonProps> = ({
-  border,
-  color,
-  children,
-  height,
-  onClick,
-  radius,
-  width,
-}) => {
-  return (
-    <button
-      onClick={onClick}
-      style={{
-        backgroundColor: color,
-        border,
-        borderRadius: radius,
-        height,
-        width,
-      }}
-    >
-      {children}
-    </button>
-  )
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>
 }
+
+// interface ButtonProps {
+//   border: string
+//   color: string
+//   children?: React.ReactNode
+//   height: string
+//   onClick: () => void
+//   radius: string
+//   width: string
+// }
+
+// const Button: React.FC<ButtonProps> = ({
+//   border,
+//   color,
+//   children,
+//   height,
+//   onClick,
+//   radius,
+//   width,
+// }) => {
+//   return (
+//     <button
+//       onClick={onClick}
+//       style={{
+//         backgroundColor: color,
+//         border,
+//         borderRadius: radius,
+//         height,
+//         width,
+//       }}
+//     >
+//       {children}
+//     </button>
+//   )
+// }
 
 export default Button
